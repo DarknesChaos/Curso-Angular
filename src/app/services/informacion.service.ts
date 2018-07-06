42d57a29fb6c7ff31b58b3677a75f3a5907c5ea5
pjavier <pjavier@messoft.com>
2018-07-06 17:02:16 -0500

    get information from firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -9,18 +9,29 @@ import { HttpClient, HttpHeaders, HttpEvent } from '@angular/common/http';
 export class InformacionService {
   info: any = {};
   cargada = false;
+  cargada_sobre_nosotros = false;
+  equipo: any = [];
 
   constructor( private httpC: HttpClient) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.httpC.get('assets/data/info.pagina.json')
       .subscribe( data => {
-        console.log(data);
+        /*console.log(data);*/
         this.info = data;
         this.cargada = true;
       });
+  }
 
-    /*this.http.get('assets/data/info.pagina.json')
+  public carga_sobre_nosotros() {
+    this.httpC.get('https://paginweb-8c882.firebaseio.com/equipo.json')
       .subscribe( data => {
-        console.log(data.json());
-      });*/
+        console.log(data);
+        this.equipo = data;
+        this.cargada_sobre_nosotros = true;
+      });
   }
 }
